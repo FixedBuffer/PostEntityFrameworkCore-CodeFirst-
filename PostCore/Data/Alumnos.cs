8a44c7f1ea00a054df0fd13b13ba020a7bfbde00
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-16 22:56:39 +0200

    Añado comentarios a Alumnos.cs

--- PostCore/Data/Alumnos.cs
+++ PostCore/Data/Alumnos.cs
@@ -5,14 +5,15 @@ using System.Text;
 
 namespace PostCore.Data
 {
-  public class Alumnos
-  {
-    [Key]
-    public int IdAlumno { get; set; }
-    public string Nombre { get; set; }
-    public DateTime Nacimiento { get; set; }
-    public int IdCurso { get; set; }
+    public class Alumnos
+    {
+        [Key] //Requiere de añadir using System.ComponentModel.DataAnnotations;
+        public int IdAlumno { get; set; } //Clave primaria
+        public string Nombre { get; set; } 
+        public DateTime Nacimiento { get; set; }
+        public int IdCurso { get; set; } //Campo clave foranea
 
-    public Cursos Curso { get; set; }
-  }
+        //Entity Framewrok Core
+        public Cursos Curso { get; set; } //Objeto de navegación virtual EFC
+    }
 }
