c95000a13f2ed3b2cd123c12ba8800f646ef2f32
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-16 22:58:03 +0200

    Añado comentarios a Cursos.cs

--- PostCore/Data/Cursos.cs
+++ PostCore/Data/Cursos.cs
@@ -5,19 +5,22 @@ using System.Text;
 
 namespace PostCore.Data
 {
-  public class Cursos
-  {
-    public Cursos()
+    public class Cursos
     {
-      Alumnos = new HashSet<Alumnos>();
-    }
-    [Key]
-    public int IdCurso { get; set; }
-    public string Nombre { get; set; }
-    public string Ciudad { get; set; }
-    public int IdProfesor { get; set; }
+        //Inicializamos el objeto de navegacion virtual de Entity Framework Core
+        public Cursos()
+        {
+            Alumnos = new HashSet<Alumnos>();
+        }
 
-    public Profesores Profesor { get; set; }
-    public ICollection<Alumnos> Alumnos { get; set; }
-  }
+        [Key] //Requiere de añadir using System.ComponentModel.DataAnnotations;
+        public int IdCurso { get; set; } //Clave primaria
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public int IdProfesor { get; set; } //Campo clave foranea
+
+        //Entity Framewrok Core
+        public Profesores Profesor { get; set; } //Objeto de navegación virtual EFC
+        public ICollection<Alumnos> Alumnos { get; set; } //Objeto de navegación virtual EFC
+    }
 }
